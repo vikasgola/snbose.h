165d5922ccea669f391ea02063648201f57b4107
Vikas Gola <vikasgola2015@gmail.com>
2023-07-29 14:55:48 +0530

    add glcheckerror

--- src/helper.h
+++ src/helper.h
@@ -4,10 +4,14 @@
 #include<GLFW/glfw3.h>
 #include<iostream>
 
-#define GLDebug(f) while(glGetError() != GL_NO_ERROR); f;\
+GLenum glCheckError_(const char *file, int line);
+
+#define glDebug(f) while(glGetError() != GL_NO_ERROR); f;\
     if(GLenum error = glGetError()){\
         std::cerr<<"OPENGL ERROR ("<<error<<"): '"<<#f<<"'"<<std::endl;\
         std::cerr<<"FILE: "<<__FILE__<<" LINE: "<<__LINE__<<std::endl;\
         glfwTerminate();\
         exit(EXIT_FAILURE);\
     }
+
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
